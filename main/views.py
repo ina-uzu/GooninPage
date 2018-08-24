4ebcae5e26dd55eb9a673b838668d5e3302bb6f6
ina-uzu <ina.uzu.song@gmail.com>
2018-08-24 16:45:20 +0900

    fix type error~~~~

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
- # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
 from django.shortcuts import render, render_to_response
@@ -10,10 +10,12 @@ from main.models import DjangoBoard
 
 
 def index(request):
-    return render(request, 'main.html',{})
+    return render(request, 'main.html', {})
 
 
-rowsPerPage=5
+rowsPerPage = 5
+
+
 def show_post_list(request):
     boardList = DjangoBoard.objects.order_by('-id')[0:5]
     current_page = 1
@@ -21,24 +23,26 @@ def show_post_list(request):
 
     pagingHelperIns = pagingHelper()
 
-    totalPageList = pagingHelper.getTotalPageList(pagingHelperIns,totalCnt,rowsPerPage)
+    totalPageList = pagingHelper.getTotalPageList(pagingHelperIns, totalCnt, rowsPerPage)
     print("totalPageList ", totalPageList)
-    return render_to_response('boardMain.html', {'boardList':boardList, 'totalCnt': totalCnt ,
-                                                        'current_page':current_page, 'totalPageList':totalPageList})
+    return render_to_response('boardMain.html', {'boardList': boardList, 'totalCnt': totalCnt,
+                                                 'current_page': current_page, 'totalPageList': totalPageList})
+
 
 class pagingHelper:
-    def getTotalPageList(self,total_cnt, rowsPerPage):
-        if(total_cnt%rowsPerPage ==0):
-            self.total_pages=total_cnt/rowsPerPage
+    def getTotalPageList(self, total_cnt, rowsPerPage):
+        if (total_cnt % rowsPerPage == 0):
+            self.total_pages = total_cnt / rowsPerPage
             print('getTotalPage #1')
 
         else:
-            self.total_pages = (total_cnt/rowsPerPage)+1;
+            self.total_pages = (total_cnt / rowsPerPage) + 1;
             print('getTotalPage #2')
 
         self.totalPageList = []
-        for i in range(self.total_pages):
-            self.totalPageList.append(i+1)
+
+        for i in range(int(self.total_pages)):
+            self.totalPageList.append(i + 1)
 
         return self.totalPageList
 
@@ -46,49 +50,49 @@ class pagingHelper:
         self.total_pages = 0
         self.totalPageList = 0
 
+
 def show_write_form(request):
     page_title = 'Write a post'
-    return render(request, 'boardWrite.html',{ 'page_title' :page_title} )
+    return render(request, 'boardWrite.html', {'page_title': page_title})
+
 
 def add_post(request):
-    
-    if request.POST.has_key('name') ==False:
-	return HttpResponse('당신은 외계인인가요! 이름이 뭐에요!')
+    if not request.POST.has_key('name'):
+        return HttpResponse('당신은 외계인인가요! 이름이 뭐에요!')
     else:
-	if len(request.POST['name']) ==0:
-	    return HttpResponse('이름이 null이신가요 그치만 그건 별로!')
-	else:
-	    entry_name = request.POST['name']
+        if len(request.POST['name']) == 0:
+            return HttpResponse('이름이 null이신가요 그치만 그건 별로!')
+        else:
+            entry_name = request.POST['name']
 
-
-    if request.POST.has_key('title') ==False:
-	return HttpResponse('제목이 울고 있어요! 만들어주세요!')
+    if not request.POST.has_key('title'):
+        return HttpResponse('제목이 울고 있어요! 만들어주세요!')
     else:
-	if len(request.POST['title']) ==0:
-	    return HttpResponse('제목이 너무 가난해요! 한톨이라도 적어주세요')
-	else:
-	    entry_title = request.POST['title']
+        if len(request.POST['title']) == 0:
+            return HttpResponse('제목이 너무 가난해요! 한톨이라도 적어주세요')
+        else:
+            entry_title = request.POST['title']
 
-    if request.POST.has_key('contents')==False:
-	return HttpResponse('내용이 울고 있어요! 만들어주세요!')
+    if not request.POST.has_key('contents'):
+        return HttpResponse('내용이 울고 있어요! 만들어주세요!')
     else:
-	if len(request.POST['contents']) ==0:
-	    return HttpResponse('내용이 너무 가난해요! 한톨이라도 적어주세요')
-	else:
-	    entry_contents = request.POST['contents']
+        if len(request.POST['contents']) == 0:
+            return HttpResponse('내용이 너무 가난해요! 한톨이라도 적어주세요')
+        else:
+            entry_contents = request.POST['contents']
 
-    new_entry = DjangoBoard(subject= entry_title, contents=entry_contents, name = entry_name)
-    
-    try:    
-	new_entry.save()
+    new_entry = DjangoBoard(subject=entry_title, contents=entry_contents, name=entry_name)
+
+    try:
+        new_entry.save()
     except:
-	return HttpResponse('미안합니다 데헤헤 글이 안 올라갈 것 같습니다!')
-    
+        return HttpResponse('미안합니다 데헤헤 글이 안 올라갈 것 같습니다!')
+
     return HttpResponse('고생하셨습니다! 글이 올라갔습니다루  뚜따띠!')
- 
+
+
 def show_read_form(request, pk):
     post = DjangoBoard.objects.get(pk=pk)
     return render(request, 'boardRead.html', {
-        'post':post
+        'post': post
     })
-
--- uzu/settings.py
+++ uzu/settings.py
@@ -119,8 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[
-    BASE_DIR+'/main/static/'
-]
-
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
